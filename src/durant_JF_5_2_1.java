95c6c8cdece7aca4367c7053bd43c6fcae0d900f
Durant <gunnar.durant@protonmail.com>
2026-06-08 13:52:07 -0700

    added do/while loop to loop through program until user exits.
    need to fix array going out of bounds.

--- src/durant_JF_5_2_1.java
+++ src/durant_JF_5_2_1.java
@@ -5,29 +5,55 @@ public class durant_JF_5_2_1 {
         Scanner keyboard = new Scanner(System.in);
         int counter = 0; //for user input
         int userInput;
+        int choice;
+        boolean validExit;
         char message[] = new char[10];
 
 
-        System.out.println("Enter 10 numbers\n" + "Your choices are:\n");
-        System.out.println("1 = D\n" + "2 = W\n" + "3 = E\n" + "4 = L\n");
+        System.out.println("Enter 10 numbers\n" + "Your choices are:");
+        System.out.println("1 = D\n" + "2 = W\n" + "3 = E\n" + "4 = L");
         System.out.println("5 = H\n" + "6 = O\n" + "7 = R\n");
 
-        for (int i = 1 ; i <=10; i ++){
-            System.out.println("Enter number "+ i + ": ");
-            userInput = keyboard.nextInt();
-            if (userInput == 1){}
-            else if (userInput == 2){message[i] = 'D';}
-            else if (userInput == 3){message[i] = 'W';}
-            else if (userInput == 4){message[i] = 'E';}
-            else if (userInput == 5){message[i] = 'H';}
-            else if (userInput == 6){message[i] = 'O';}
-            else if (userInput == 7){message[i] = 'R';}
-            else {
-                System.out.println("Invalid number!, Please try again");
-                i--; //to remove incorrect i++ in loop
+        do {
+            for (int i = 1; i < 10; i++) {
+                System.out.println("Enter number " + i + ": ");
+                userInput = keyboard.nextInt();
+                if (userInput == 1) {
+                    message[i] = 'D';}
+                else if (userInput == 2) {
+                    message[i] = 'W';}
+                else if (userInput == 3) {
+                    message[i] = 'E';}
+                else if (userInput == 4) {
+                    message[i] = 'L';}
+                else if (userInput == 5) {
+                    message[i] = 'H';}
+                else if (userInput == 6) {
+                    message[i] = 'O';}
+                else if (userInput == 7) {
+                    message[i] = 'R';}
+                else {
+                    System.out.println("Invalid number!, Please try again");
+                    i--; //to remove incorrect i++ in loop
+                }
+
+            }
+            System.out.println("Message: ");
+            for (int i = 0; i <= message.length; i++) {
+                System.out.println(message[i]);
             }
 
+            System.out.println("Enter 1 to go again.");
+            System.out.println("Enter 2 to Exit program.");
+            choice = keyboard.nextInt();
 
-        }
+            if (choice == 1) {
+                validExit = true;}
+            else {
+                validExit = false;
+            }
+
+        }while(validExit);
+
     }
 }
