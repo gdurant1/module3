d4a629ab6a04754bb18cec717f3c24dbf71f0fc1
Durant <gunnar.durant@protonmail.com>
2026-06-08 12:49:15 -0700

    5_2_1: started base code

--- src/durant_JF_5_2_1.java
+++ src/durant_JF_5_2_1.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
 public class durant_JF_5_2_1 {
+    static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        int counter = 0; //for user input
+
+        for (int i = 0 ; i <=10; i ++){
+            System.out.println("Enter 10 numbers\n" + "Your choices are:\n");
+            System.out.println("1 = D\n" + "2 = W\n" + "3 = E\n" + "4 = L\n");
+            System.out.println("5 = H\n" + "6 = O\n" + "7 = R\n");
+            //holder for saving user input
+        }
+    }
 }
