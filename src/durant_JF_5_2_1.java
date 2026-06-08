f8f9c59697dffa1a55bd10636b8127751a693266
Durant <gunnar.durant@protonmail.com>
2026-06-08 13:37:59 -0700

    5_2_1: completed adding to array included else with correction for any wrong input

--- src/durant_JF_5_2_1.java
+++ src/durant_JF_5_2_1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.String;
 public class durant_JF_5_2_1 {
     static void main(String[] args){
         Scanner keyboard = new Scanner(System.in);
@@ -15,12 +16,16 @@ public class durant_JF_5_2_1 {
             System.out.println("Enter number "+ i + ": ");
             userInput = keyboard.nextInt();
             if (userInput == 1){}
-            else if (userInput == 2){message[i] = "D";}
-            else if (userInput == 3){message=("W");}
-            else if (userInput == 4){message=add("E");}
-            else if (userInput == 5){message=add("H");}
-            else if (userInput == 6){message=add("O");}
-            else if (userInput == 7){message=add("R");}
+            else if (userInput == 2){message[i] = 'D';}
+            else if (userInput == 3){message[i] = 'W';}
+            else if (userInput == 4){message[i] = 'E';}
+            else if (userInput == 5){message[i] = 'H';}
+            else if (userInput == 6){message[i] = 'O';}
+            else if (userInput == 7){message[i] = 'R';}
+            else {
+                System.out.println("Invalid number!, Please try again");
+                i--; //to remove incorrect i++ in loop
+            }
 
 
         }
